ee074797cdd5cbb39eeb227ce1952a5b7268ef23
Hertach Manuel <manuel@hertach.org>
2014-09-17 16:11:03 +0200

    Grunt

--- system/core/index.php
+++ system/core/index.php
@@ -6,6 +6,12 @@
     <link href="css/style.css" rel="stylesheet"
 </head>
 <body>
-    test
+    <div class="row">
+        <div class="small-12 columns">12 columns</div>
+    </div>
+    <div class="row">
+        <div class="small-3 columns">3 columns</div>
+        <div class="small-9 columns">9 columns</div>
+    </div>
 </body>
 </html>
